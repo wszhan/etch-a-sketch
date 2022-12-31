10d4991c58c1f450f9a7ac8838041ae2ae740dc6
wszhan <wszhan@@users.noreply.github.com>
2022-12-31 00:18:05 -0500

    generate arbitrary length grid; render random color while hovering

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-const body = document.querySelector('body');
-const container = document.querySelector('div.container');
+const body = document.querySelector("body");
+const container = document.querySelector("div.container");
 
 createGrid(1); // init
 
-const btn = document.querySelector('button');
+const colors = ['#ff0000', '#00ff00', '#0000ff'];
 
-btn.addEventListener('click', (e) => {
-    const x = prompt('What do you want the dimension of the grid to be?'); 
-    createGrid(x);
+const btn = document.querySelector("button");
+
+function renderRandomColor(gridItem) {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    gridItem.style.backgroundColor = randomColor;
+}
+
+btn.addEventListener("click", (e) => {
+  const x = prompt("What do you want the dimension of the grid to be?");
+  createGrid(x);
 });
 
 function createGrid(dimension) {
-    container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
-    // container.style.gridTemplateRows = `repeat(${x}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
 
-    const total = dimension * dimension;
+  const total = dimension * dimension;
 
-    container.innerHTML = "";
+  container.innerHTML = "";
 
-    for (let i = 0; i < total; i++) {
-    const newItem = document.createElement('div');
-    newItem.setAttribute('id', `item-${i}`);
-    newItem.classList.add('grid-item');
-    newItem.innerText = `grid-item-${i}`;
-    
-    newItem.addEventListener('mouseover', (e) => {
-        newItem.style.backgroundColor = "yellow";
+  for (let i = 0; i < total; i++) {
+    const newItem = document.createElement("div");
+    newItem.setAttribute("id", `item-${i}`);
+    newItem.classList.add("grid-item");
+
+    newItem.addEventListener("mouseover", (e) => {
+        renderRandomColor(newItem);
     });
 
     container.appendChild(newItem);
-    }
+  }
 }
